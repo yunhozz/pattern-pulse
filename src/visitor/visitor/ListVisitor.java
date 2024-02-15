788e1e54be1ef4704284f3b80c559a4ddc128d20
yunhozz <qkrdbsgh1121@naver.com>
2024-02-15 17:43:13 +0900

    Visitor Pattern

--- src/visitor/visitor/ListVisitor.java
+++ src/visitor/visitor/ListVisitor.java
@@ -1,11 +1,14 @@
 package visitor.visitor;
 
 import visitor.element.Directory;
+import visitor.element.Entry;
 import visitor.element.File;
+
+import java.util.Iterator;
 
 public class ListVisitor extends Visitor {
 
-    private final String currentDir = ""; // 현재 주목하고 있는 디렉토리 이름 저장하는 필드
+    private String currentDir = ""; // 현재 주목하고 있는 디렉토리 이름 저장하는 필드
 
     @Override
     public void visit(File file) {
@@ -14,7 +17,17 @@ public class ListVisitor extends Visitor {
 
     @Override
     public void visit(Directory directory) {
+        System.out.println(currentDir + "/" + directory);
+        String savedir = currentDir;
 
+        currentDir = currentDir + "/" + directory.getName();
+        Iterator it = directory.iterator();
+
+        while (it.hasNext()) {
+            Entry entry = (Entry) it.next();
+            entry.accept(this);
+        }
+        currentDir = savedir;
     }
 
     public String getCurrentDir() {
